312bc49f8628ef56141b1fc1eddfc928d0413820
Fred Goldstein-Rose <goldsteinrose@cs.uchicago.edu>
2020-01-18 13:24:41 -0600

    omg it works

--- keybird/app/src/main/java/com/example/keybird/KeyboardIME.java
+++ keybird/app/src/main/java/com/example/keybird/KeyboardIME.java
@@ -7,6 +7,7 @@ import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
@@ -15,24 +16,10 @@ public class KeyboardIME extends InputMethodService {
     private MyKeyboardView mInputView;
 
     @Override
-    public void onCreate(){
-        super.onCreate();
-
-    }
-    @Override
     public View onCreateInputView(){
-
-        //mInputView = new MyKeyboardView(this, null);
-        mInputView = (MyKeyboardView) getLayoutInflater().inflate(R.layout.input, null);
-        return mInputView;
-        //Log.d("height", Integer.toString(mInputView.getMeasuredHeight()));
-        //mInputView.setOnTouchListener(new MyTouchListener(mInputView, getCurrentInputConnection()));
-
-        //return mInputView;
+        View layout = getLayoutInflater().inflate(R.layout.input, null);
+        mInputView = layout.findViewById(R.id.kbv);
+        mInputView.setOnTouchListener(new MyTouchListener(mInputView, getCurrentInputConnection()));
+        return layout;
     }
-
-/*
-    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
-        outAttrs.imeOptions = EditorInfo.IME_FLAG_NO_EXTRACT_UI;
-    }*/
 }
