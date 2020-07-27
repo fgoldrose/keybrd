f04bbec39a203e208acfc10ab88fab3c98e7b453
Fred Goldstein-Rose <goldsteinrose@cs.uchicago.edu>
2020-07-27 00:26:11 -0400

    redesigned touch listening, fixed persistence bug. Added backspace

--- keybird/app/src/main/java/com/example/keybird/KeyboardIME.java
+++ keybird/app/src/main/java/com/example/keybird/KeyboardIME.java
@@ -1,30 +1,23 @@
 package com.example.keybird;
 
 import android.inputmethodservice.InputMethodService;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputConnection;
-import android.inputmethodservice.KeyboardView;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.support.v7.app.AppCompatActivity;
 
-public class KeyboardIME extends InputMethodService {
-    private MyKeyboardView mInputView;
 
-    @Override
-    public void onCreate(){
-        super.onCreate();
+public class KeyboardIME extends InputMethodService implements MyKeyboardView.MyListener {
 
-    }
     @Override
     public View onCreateInputView(){
         View layout = getLayoutInflater().inflate(R.layout.input, null);
-        mInputView = layout.findViewById(R.id.kbv);
-        mInputView.setOnTouchListener(new MyTouchListener(mInputView, getCurrentInputConnection()));
+        MyKeyboardView kb = layout.findViewById(R.id.kbv);
+        kb.setListener(this);
         return layout;
+    }
+
+    public void onKey(String text){
+        getCurrentInputConnection().commitText(text, 1);
+    }
+    public void onBackspace(){
+        getCurrentInputConnection().deleteSurroundingText(1,0);
     }
 }
--- keybird/app/src/main/java/com/example/keybird/MyKeyboardView.java
+++ keybird/app/src/main/java/com/example/keybird/MyKeyboardView.java
@@ -1,57 +1,104 @@
 package com.example.keybird;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
-import android.inputmethodservice.KeyboardView;
-import android.widget.RelativeLayout;
-
 
 public class MyKeyboardView extends View {
-
-    RectF kb;
-    float outerpercent;
-    float innerpercent;
-    int highlighted;
-    int mode;
-    float buttonpercent;
-
     public MyKeyboardView(Context context, @Nullable AttributeSet attrs){
         super(context, attrs);
-        init(attrs);
     }
     public MyKeyboardView(Context context, @Nullable AttributeSet attrs, int defStyleAttr){
         super(context, attrs, defStyleAttr);
-        init(attrs);
     }
 
-    private void init(@Nullable AttributeSet set){
-        kb = new RectF();
-        highlighted=-1;
-        mode = 0;
+    // size of
+    float outerpercent = (float) 0.7;
+    float innerpercent = (float) 0.5;
+    float buttonpercent = (float) 0.2;
 
-        if(set == null){
-            return;
+    int highlighted = -1;
+    int mode = 0;
+    private int start = -1;
+    double longpresstime = 500;
+
+    private MyListener listener = null;
+
+    public interface MyListener {
+        void onKey(String text);
+        void onBackspace();
+    }
+
+
+    public void setListener(MyListener listener){
+        this.listener = listener;
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float outerradius = outerpercent * getHeight()/2;
+        float innerradius = innerpercent * outerradius;
+        float buttonradius = buttonpercent * getHeight()/2;
+        float centerx = getWidth() /2;
+        float centery = getHeight() /2;
+        int seg = getSegment(event.getX(), event.getY(),
+                centerx, centery, outerradius, innerradius, buttonradius);
+        if(event.getAction() == MotionEvent.ACTION_UP){
+            if(start == 0 && seg == 0){
+                // Center button
+
+                if(event.getEventTime() - event.getDownTime() >= longpresstime){
+                    // Trigger mode change
+                    if(mode == 0){
+                        mode = 1;
+                    }
+                    else if(mode == 1){
+                        mode = 0;
+                    }
+                }
+                else {
+                    if(listener != null)
+                        listener.onKey(" ");
+                }
+            }
+            start = -1;
+            this.highlighted = start;
+            this.invalidate();
         }
-        innerpercent= (float) 0.5;
-        outerpercent= (float)  0.7;
-        buttonpercent= (float) 0.2;
-        //TypedArray ta = getContext().obtainStyledAttributes(set, R.styleable.MyKeyboardView);
-        //outerpercent = ta.getFloat(R.styleable.MyKeyboardView_outer_percent, 1);
-        //innerpercent = ta.getFloat(R.styleable.MyKeyboardView_inner_percent, 1/2);
-        // buttonpercent = ta.getFloat(R.styleable.MyKeyboardView_button_percent, 1/4);
-        //ta.recycle();
+        else {
+            if (start == -1) {
+                // First button touched
+                start = seg;
+            }
+            else if (start != seg && seg != 0 && seg != -1) {
+                // Second button touched
+
+                if(start == 0 && seg == 7 && listener != null){
+                    listener.onBackspace();
+                }
+                else if (listener != null){
+                    listener.onKey(charCode(start, seg));
+                }
+                start = seg;
+            }
+
+            this.highlighted = start;
+            this.invalidate();
+        }
+        return true;
     }
 
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         float outerradius = outerpercent * getHeight()/2;
         float innerradius = innerpercent * outerradius;
         float buttonradius = buttonpercent * getHeight()/2;
@@ -96,26 +143,6 @@ public class MyKeyboardView extends View {
         Paint pc = new Paint(Paint.ANTI_ALIAS_FLAG);
         pc.setStyle(Paint.Style.STROKE);
 
-        /*
-        Paint p1 = new Paint(Paint.ANTI_ALIAS_FLAG);
-        p1.setColor(Color.GREEN);
-        Paint p2 = new Paint(Paint.ANTI_ALIAS_FLAG);
-        p2.setColor(Color.BLUE);
-        Paint p3 = new Paint(Paint.ANTI_ALIAS_FLAG);
-        p3.setColor(Color.RED);
-        Paint p4 = new Paint(Paint.ANTI_ALIAS_FLAG);
-        p4.setColor(Color.MAGENTA);
-        Paint p5 = new Paint(Paint.ANTI_ALIAS_FLAG);
-        p5.setColor(Color.YELLOW);
-        Paint p6 = new Paint(Paint.ANTI_ALIAS_FLAG);
-        p6.setColor(Color.CYAN);
-        Paint p7 = new Paint(Paint.ANTI_ALIAS_FLAG);
-        p7.setColor(Color.DKGRAY);
-        Paint p8 = new Paint(Paint.ANTI_ALIAS_FLAG);
-        p8.setColor(Color.LTGRAY);
-        Paint pc = new Paint(Paint.ANTI_ALIAS_FLAG);
-        pc.setColor(Color.GRAY);
-        */
         canvas.drawCircle(x1, y1, buttonradius, p1);
         canvas.drawCircle(x2, y2, buttonradius, p2);
         canvas.drawCircle(x3, y3, buttonradius, p3);
@@ -160,70 +187,17 @@ public class MyKeyboardView extends View {
         float textOffset = (textHeight / 2) - textpaint.descent();
 
 
-        canvas.drawText(Character.toString(charCode(highlighted, 1)), x1, y1 + textOffset, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 2)), x2, y2 + textOffset, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 3)), x3, y3 + textOffset, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 4)), x4, y4 + textOffset, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 5)), x5, y5 + textOffset, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 6)), x6, y6 + textOffset, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 7)), x7, y7 + textOffset, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 8)), x8, y8 + textOffset, textpaint);
-
-
-        // BELOW is the old code to draw segments!
-        /*
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(Color.GRAY);
-        RectF kb = new RectF();
-        kb.left = centerx - outerradius;
-        kb.right = centerx + outerradius;
-        kb.top = centery - outerradius;
-        kb.bottom = centery + outerradius;
-
-        Paint segpaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        segpaint.setColor(Color.LTGRAY);
-        Paint centpaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        centpaint.setColor(Color.WHITE);
-        Paint hlpaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        if(mode == 0){
-            hlpaint.setColor(Color.BLUE);
-        }
-       else{
-           hlpaint.setColor(Color.RED);
-        }
-
-        canvas.drawOval(kb, paint);
-        canvas.drawArc(kb, 22.5F, 45F, true, segpaint);
-        canvas.drawArc(kb, 112.5F, 45F, true, segpaint);
-        canvas.drawArc(kb, 202.5F, 45F, true, segpaint);
-        canvas.drawArc(kb, 292.5F, 45F, true, segpaint);
-        if (highlighted >0) {
-            float startangle = (float) 112.5 - 45 * highlighted;
-            canvas.drawArc(kb, startangle, 45F, true, hlpaint);
-        }
-        if(highlighted == 0){
-            canvas.drawCircle(centerx, centery, innerradius, hlpaint);
-        }
-        else{
-            canvas.drawCircle(centerx, centery, innerradius, centpaint);
-        }
-
-        Paint textpaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        textpaint.setColor(Color.BLACK);
-        textpaint.setTextSize(60);
-
-        canvas.drawText(Character.toString(charCode(highlighted, 1)), centerx, centery + textrad, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 2)), centerx + angle * textrad, centery + angle * textrad, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 3)), centerx + textrad, centery, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 4)), centerx + angle * textrad, centery - angle * textrad, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 5)), centerx, centery - textrad, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 6)), centerx - angle * textrad, centery - angle * textrad, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 7)), centerx - textrad, centery, textpaint);
-        canvas.drawText(Character.toString(charCode(highlighted, 8)), centerx - angle * textrad, centery + angle * textrad, textpaint);
-        */
+        canvas.drawText(charCode(highlighted, 1), x1, y1 + textOffset, textpaint);
+        canvas.drawText(charCode(highlighted, 2), x2, y2 + textOffset, textpaint);
+        canvas.drawText(charCode(highlighted, 3), x3, y3 + textOffset, textpaint);
+        canvas.drawText(charCode(highlighted, 4), x4, y4 + textOffset, textpaint);
+        canvas.drawText(charCode(highlighted, 5), x5, y5 + textOffset, textpaint);
+        canvas.drawText(charCode(highlighted, 6), x6, y6 + textOffset, textpaint);
+        canvas.drawText(charCode(highlighted, 7), x7, y7 + textOffset, textpaint);
+        canvas.drawText(charCode(highlighted, 8), x8, y8 + textOffset, textpaint);
     }
 
-    public char charCode(int from, int to) {
+    public String charCode(int from, int to) {
         int code;
         if (from < to) {
             code = mode * 100 + from * 10 + to;
@@ -232,130 +206,121 @@ public class MyKeyboardView extends View {
         }
         switch(code){
             case 0:
-                return ' ';
+                return " ";
             case 1:
-                return '.';
+                return ".";
             case 2:
-                return '?';
-            case 8:
-                return ',';
+                return "?";
+            case 3:
+                return "!";
             case 7:
-                return '!';
+                return "<--";
+            case 8:
+                return ",";
 
             case 13:
-                return 'y';
+                return "y";
             case 24:
-                return 'w';
+                return "w";
             case 34:
-                return 'q';
+                return "q";
             case 16:
-                return 'r';
+                return "r";
             case 28:
-                return 'z';
+                return "z";
             case 46:
-                return 'd';
+                return "d";
             case 57:
-                return 's';
+                return "s";
             case 12:
-                return 'a';
+                return "a";
             case 78:
-                return 'k';
+                return "k";
             case 35:
-                return 'u';
+                return "u";
             case 15:
-                return 'o';
+                return "o";
             case 47:
-                return 'v';
+                return "v";
             case 18:
-                return 'p';
+                return "p";
             case 68:
-                return 'x';
+                return "x";
             case 36:
-                return 'l';
+                return "l";
             case 56:
-                return 'i';
+                return "i";
             case 27:
-                return 'h';
+                return "h";
             case 17:
-                return 'c';
+                return "c";
             case 25:
-                return 't';
+                return "t";
             case 14:
-                return 'j';
+                return "j";
             case 58:
-                return 'm';
+                return "m";
             case 26:
-                return 'n';
+                return "n";
             case 37:
-                return 'b';
+                return "b";
             case 23:
-                return 'g';
+                return "g";
             case 67:
-                return 'e';
+                return "e";
             case 45:
-                return 'f';
-
-            /*
-            case 12:
-                return 'a';
-            case 13:
-                return 'b';
-            case 14:
-                return 'c';
-            case 15:
-                return 'd';
-            case 16:
-                return 'e';
-            case 17:
-                return 'f';
-            case 18:
-                return 'g';
-            case 23:
-                return 'h';
-            case 24:
-                return 'i';
-            case 25:
-                return 'j';
-            case 26:
-                return 'k';
-            case 27:
-                return 'l';
-            case 28:
-                return 'm';
-            case 34:
-                return 'n';
-            case 35:
-                return 'o';
-            case 36:
-                return 'p';
-            case 37:
-                return 'q';
-            case 38:
-                return 'r';
-            case 45:
-                return 's';
-            case 46:
-                return 't';
-            case 47:
-                return 'u';
-            case 48:
-                return 'v';
-            case 56:
-                return 'w';
-            case 57:
-                return 'x';
-            case 58:
-                return 'y';
-            case 67:
-                return 'z';
-            case 68:
-                return '\0';
-            case 78:
-                return '\0';
-             */
+                return "f";
 
             default:
-                return '\0';
+                return "";
+        }
+    }
+
+
+    private double distance(double x1, double y1, double x2, double y2){
+        return Math.sqrt(Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2));
+    }
+
+    private int getSegment(double xtouch, double ytouch, double centerx, double centery, double outerrad, double innerrad, double buttonrad) {
+        double textrad = outerrad;
+        double angle = 1 / (float) Math.sqrt(2);
+        double x1 = centerx;
+        double y1 = centery + textrad;
+        double x2 = centerx + angle * textrad;
+        double y2 = centery + angle * textrad;
+        double x3 = centerx + textrad;
+        double y3 = centery;
+        double x4 = centerx + angle * textrad;
+        double y4 = centery - angle * textrad;
+        double x5 = centerx;
+        double y5 = centery - textrad;
+        double x6 = centerx - angle * textrad;
+        double y6 = centery - angle * textrad;
+        double x7 = centerx - textrad;
+        double y7 = centery;
+        double x8 = centerx - angle * textrad;
+        double y8 = centery + angle * textrad;
+        double cdist = distance(xtouch, ytouch, centerx, centery);
+        if (cdist < innerrad) {
+            return 0; // inside inner radius of circle
+        } else if (distance(xtouch, ytouch, x1, y1) <= buttonrad) {
+            return 1;
+        } else if (distance(xtouch, ytouch, x2, y2) <= buttonrad) {
+            return 2;
+        } else if (distance(xtouch, ytouch, x3, y3) <= buttonrad) {
+            return 3;
+        } else if (distance(xtouch, ytouch, x4, y4) <= buttonrad) {
+            return 4;
+        } else if (distance(xtouch, ytouch, x5, y5) <= buttonrad) {
+            return 5;
+        } else if (distance(xtouch, ytouch, x6, y6) <= buttonrad) {
+            return 6;
+        } else if (distance(xtouch, ytouch, x7, y7) <= buttonrad) {
+            return 7;
+        } else if (distance(xtouch, ytouch, x8, y8) <= buttonrad) {
+            return 8;
+        } else {
+            return -1;
         }
     }
 }
